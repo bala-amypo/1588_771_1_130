fa26fa7a7bf02f831c543ff474825c1ff7a6171c
bala-amypo <bala@amypo.in>
2025-12-16 10:35:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/StolenDeviceReport.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+public class StolenDeviceReport {
+    private long id;
+    private String serialNumber;
+    private String reportedBy;
+    private LocalDateTime reportDate;
+    private String details;
+    
+}
