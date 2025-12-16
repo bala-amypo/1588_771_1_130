5f846e4d262228a7b026eb71dbcfbfeaed9af74c
bala-amypo <bala@amypo.in>
2025-12-16 16:55:28 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
+++ src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
@@ -1,4 +1,4 @@
-package com.example.demo.model;
+package com.example.project.models;
 
 import java.time.LocalDate;
 
@@ -6,10 +6,67 @@ public class DeviceOwnershipRecord {
     private  long id;
     private String serialNumber;
     private String ownerName;
-    private String ownerString;
+    private String ownerEmail;
     private LocalDate purchaseDate;
     private LocalDate warrantyExpiration;
     private Boolean active;
     
-    
+    public DeviceOwnershipRecord() {}
+
+    public DeviceOwnershipRecord(String serialNumber,String ownerName,String ownerEmail,LocalDate puchaseDate,LocalDate warrantyExpiration,Boolean active) {
+        this.serialNumber=serialNumber;
+        this.ownerName=ownerName;
+        this.ownerEmail=ownerEmail;
+        this.purchaseDate=purchaseDate;
+        this.warrantyExpiration=warrantyExpiration;
+        this.active=active;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public LocalDate getWarrantyExpiration() {
+        return warrantyExpiration;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public void setWarrantyExpiration(LocalDate warrantyExpiration) {
+        this.warrantyExpiration = warrantyExpiration;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
