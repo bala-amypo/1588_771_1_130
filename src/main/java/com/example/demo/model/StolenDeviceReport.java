61db4f7db0277543433438822f70122af3a3acae
bala-amypo <bala@amypo.in>
2025-12-16 17:15:18 +0000

    User commit

--- src/main/java/com/example/demo/model/StolenDeviceReport.java
+++ src/main/java/com/example/demo/model/StolenDeviceReport.java
@@ -1,4 +1,4 @@
-package com.example.demo.model;
+package com.example.project.models;
 
 import java.time.LocalDateTime;
 
@@ -8,5 +8,46 @@ public class StolenDeviceReport {
     private String reportedBy;
     private LocalDateTime reportDate;
     private String details;
+    
+    public StolenDeviceReport() {}
+
+    public StolenDeviceReport(String serialNumber,String reportedBy,LocalDateTime reportDate,String details) {
+        this.serialNumber=serialNumber;
+        this.reportedBy=reportedBy;
+        this.reportDate=reportDate;
+        this.details=details;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getReportedBy() {
+        return reportedBy;
+    }
+
+    public LocalDateTime getReportDate() {
+        return reportDate;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setReportedBy(String reportedBy) {
+        this.reportedBy = reportedBy;
+    }
+
+    public void setReportDate(LocalDateTime reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
     
 }
