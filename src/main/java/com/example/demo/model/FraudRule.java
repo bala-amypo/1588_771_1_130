6d5bf2630c58cd5a911442aa81f7367b64db71c7
bala-amypo <bala@amypo.in>
2025-12-16 10:24:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/FraudRule.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class FraudRule {
+    private long id;
+    private String ruleCode;
+    private String description;
+    private String ruleType;
+    private Boolean active;
+}
