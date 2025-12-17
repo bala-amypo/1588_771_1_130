553895609e65fccb6478859a53bca6b2cb8533c3
bala-amypo <bala@amypo.in>
2025-12-17 04:16:42 +0000

    User commit

--- src/main/java/com/example/demo/model/FraudRule.java
+++ src/main/java/com/example/demo/model/FraudRule.java
@@ -6,4 +6,45 @@ public class FraudRule {
     private String description;
     private String ruleType;
     private Boolean active;
+
+    public FraudRule() {}
+
+    public FraudRule(String ruleCode,String description,String ruleType,Boolean active) {
+        this.ruleCode=ruleCode;
+        this.description=description;
+        this.ruleType=ruleType;
+        this.active=active;
+    }
+
+    public String getRuleCode() {
+        return ruleCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getRuleType() {
+        return ruleType;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setRuleCode(String ruleCode) {
+        this.ruleCode = ruleCode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setRuleType(String ruleType) {
+        this.ruleType = ruleType;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
