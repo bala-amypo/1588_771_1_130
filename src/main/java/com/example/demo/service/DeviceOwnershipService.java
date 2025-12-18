bd00a67db0c8112466a48b13da86e95242cf8075
bala-amypo <bala@amypo.in>
2025-12-18 10:08:02 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviceOwnershipService.java
+++ src/main/java/com/example/demo/service/DeviceOwnershipService.java
@@ -1,24 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.model.DeviceOwnershipRecord;
-import com.example.demo.repository.DeviceOwnershipRecordRepository;
-
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@Service
-public class DeviceOwnershipService {
+public interface DeviceOwnershipService {
 
-    @Autowired
-    private DeviceOwnershipRecordRepository repo;
+    DeviceOwnershipRecord create(DeviceOwnershipRecord record);
 
-    public DeviceOwnershipRecord registerDevice(DeviceOwnershipRecord device) {
-        return repo.save(device);
-    }
-
-    public List<DeviceOwnershipRecord> getAllDevices() {
-        return repo.findAll();
-    }
+    List<DeviceOwnershipRecord> getAll();
 }
