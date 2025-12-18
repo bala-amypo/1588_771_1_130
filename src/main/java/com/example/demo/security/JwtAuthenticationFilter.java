55b7765129cb02126beafb85384c6482a37be594
bala-amypo <bala@amypo.in>
2025-12-18 08:20:16 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,17 +1,17 @@
-@Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+// @Component
+// public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtTokenProvider jwtProvider;
+//     @Autowired
+//     private JwtTokenProvider jwtProvider;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
-            throws ServletException, IOException {
+//     @Override
+//     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+//             throws ServletException, IOException {
 
-        String header = req.getHeader("Authorization");
-        if (header != null && header.startsWith("Bearer ")) {
-            jwtProvider.validateToken(header.substring(7));
-        }
-        chain.doFilter(req, res);
-    }
-}
+//         String header = req.getHeader("Authorization");
+//         if (header != null && header.startsWith("Bearer ")) {
+//             jwtProvider.validateToken(header.substring(7));
+//         }
+//         chain.doFilter(req, res);
+//     }
+// }
