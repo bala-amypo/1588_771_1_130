69a08da97a40248259ba2bc8551db4a7679cefd2
bala-amypo <bala@amypo.in>
2025-12-19 06:32:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/StolenDeviceController.java
+++ src/main/java/com/example/demo/controller/StolenDeviceController.java
@@ -1,3 +1,11 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 @RequestMapping("/api/stolen-devices")
 public class StolenDeviceController {
