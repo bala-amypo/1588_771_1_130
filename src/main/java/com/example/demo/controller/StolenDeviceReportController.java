1bce1d7cc6abbc4c555eadffa02845a870385a5b
bala-amypo <bala@amypo.in>
2025-12-19 05:07:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/StolenDeviceReportController.java
+++ src/main/java/com/example/demo/controller/StolenDeviceReportController.java
@@ -1,30 +1,30 @@
-// @RestController
-// @RequestMapping("/api/stolen-devices")
-// public class StolenDeviceController {
+@RestController
+@RequestMapping("/api/stolen-devices")
+public class StolenDeviceController {
 
-//     private final StolenDeviceService service;
+    private final StolenDeviceService service;
 
-//     public StolenDeviceController(StolenDeviceService service) {
-//         this.service = service;
-//     }
+    public StolenDeviceController(StolenDeviceService service) {
+        this.service = service;
+    }
 
-//     @PostMapping
-//     public StolenDeviceReport report(@RequestBody StolenDeviceReport report) {
-//         return service.report(report);
-//     }
+    @PostMapping
+    public StolenDeviceReport report(@RequestBody StolenDeviceReport report) {
+        return service.report(report);
+    }
 
-//     @GetMapping("/serial/{serialNumber}")
-//     public List<StolenDeviceReport> bySerial(@PathVariable String serialNumber) {
-//         return service.getBySerial(serialNumber);
-//     }
+    @GetMapping("/serial/{serialNumber}")
+    public List<StolenDeviceReport> bySerial(@PathVariable String serialNumber) {
+        return service.getBySerial(serialNumber);
+    }
 
-//     @GetMapping("/{id}")
-//     public StolenDeviceReport byId(@PathVariable Long id) {
-//         return service.getById(id);
-//     }
+    @GetMapping("/{id}")
+    public StolenDeviceReport byId(@PathVariable Long id) {
+        return service.getById(id);
+    }
 
-//     @GetMapping
-//     public List<StolenDeviceReport> all() {
-//         return service.getAll();
-//     }
-// }
+    @GetMapping
+    public List<StolenDeviceReport> all() {
+        return service.getAll();
+    }
+}
