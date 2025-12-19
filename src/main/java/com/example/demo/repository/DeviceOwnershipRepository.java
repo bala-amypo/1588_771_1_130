160b1864af7e57e2a72161f14150aa70bc1464e5
bala-amypo <bala@amypo.in>
2025-12-19 10:18:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceOwnershipRepository.java
+++ src/main/java/com/example/demo/repository/DeviceOwnershipRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DeviceOwnershipRecordRepository extends JpaRepository<DeviceOwnershipRecord, Long> {
+
+    // Find a device by serial number
     Optional<DeviceOwnershipRecord> findBySerialNumber(String serialNumber);
+
+    // Check if a device with the given serial number already exists
+    boolean existsBySerialNumber(String serialNumber);
 }
