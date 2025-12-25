22d5347ac6ac3731b9100358989d062564ef6a6f
bala-amypo <bala@amypo.in>
2025-12-25 16:29:23 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviceOwnershipService.java
+++ src/main/java/com/example/demo/service/DeviceOwnershipService.java
@@ -1,16 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.model.DeviceOwnershipRecord;
-
 import java.util.List;
 
 public interface DeviceOwnershipService {
-
     DeviceOwnershipRecord registerDevice(DeviceOwnershipRecord record);
-
     List<DeviceOwnershipRecord> getAllDevices();
-
-    DeviceOwnershipRecord getDeviceById(Long id);
-
-    void deleteDevice(Long id);
 }
--- src/main/java/com/example/demo/service/StolenDeviceService.java
+++ src/main/java/com/example/demo/service/StolenDeviceService.java
@@ -1,16 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.model.StolenDeviceReport;
-
 import java.util.List;
 
 public interface StolenDeviceService {
-
-    StolenDeviceReport reportStolenDevice(StolenDeviceReport report);
-
-    List<StolenDeviceReport> getAllStolenDevices();
-
-    StolenDeviceReport getStolenDeviceById(Long id);
-
-    void deleteStolenDevice(Long id);
+    StolenDeviceReport reportStolen(StolenDeviceReport report);
+    List<StolenDeviceReport> getAllReports();
 }
