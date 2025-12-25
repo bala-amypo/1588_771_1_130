d03a3f1259737406acea3089cece2ac9a9df6d0d
bala-amypo <bala@amypo.in>
2025-12-25 17:37:56 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,12 +1,12 @@
 package com.example.demo.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class RegisterRequest {
     private String username;
     private String email;
     private String password;
 }
+
+
--- src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
+++ src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
@@ -1,12 +1,12 @@
+// Correct version
 package com.example.demo.repository;
 
 import com.example.demo.model.DeviceOwnershipRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
-public interface DeviceOwnershipRepository extends JpaRepository<DeviceOwnershipRecord, Long> {
+public interface DeviceOwnershipRecordRepository extends JpaRepository<DeviceOwnershipRecord, Long> {
     Optional<DeviceOwnershipRecord> findBySerialNumber(String serialNumber);
 }
