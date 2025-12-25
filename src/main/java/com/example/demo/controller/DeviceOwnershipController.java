19ad2fada86935bab804269403f0735e9383cc8b
bala-amypo <bala@amypo.in>
2025-12-25 16:10:40 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceOwnershipController.java
+++ src/main/java/com/example/demo/controller/DeviceOwnershipController.java
@@ -2,29 +2,25 @@ package com.example.demo.controller;
 
 import com.example.demo.model.DeviceOwnershipRecord;
 import com.example.demo.service.DeviceOwnershipService;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/devices")
+@RequiredArgsConstructor
 public class DeviceOwnershipController {
 
     private final DeviceOwnershipService service;
 
-    public DeviceOwnershipController(DeviceOwnershipService s) {
-        this.service = s;
-    }
-
-    @PostMapping
-    public DeviceOwnershipRecord register(
-            @RequestBody DeviceOwnershipRecord d) {
-        return service.registerDevice(d);
+    @PostMapping("/register")
+    public DeviceOwnershipRecord registerDevice(@RequestBody DeviceOwnershipRecord record) {
+        return service.registerDevice(record);
     }
 
     @GetMapping
-    public List<DeviceOwnershipRecord> all() {
+    public List<DeviceOwnershipRecord> getAllDevices() {
         return service.getAllDevices();
     }
 }
