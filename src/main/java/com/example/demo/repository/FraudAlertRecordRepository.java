dfd6b451043c13418b9e2d879cbb8b64a5f8ecdf
bala-amypo <bala@amypo.in>
2025-12-25 17:53:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/FraudAlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/FraudAlertRecordRepository.java
@@ -1,8 +1,13 @@
+// 2️⃣ Repository
 package com.example.demo.repository;
 
 import com.example.demo.model.FraudAlertRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface FraudAlertRepository extends JpaRepository<FraudAlertRecord, Long> {}
+public interface FraudAlertRecordRepository extends JpaRepository<FraudAlertRecord, Long> {
+    List<FraudAlertRecord> findByResolved(boolean resolved);  // custom query example
+}
