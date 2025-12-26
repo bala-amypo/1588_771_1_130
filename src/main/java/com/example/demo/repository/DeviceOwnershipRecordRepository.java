ebb911af570b071bbb7a20ac45e370741cb3b67f
bala-amypo <bala@amypo.in>
2025-12-26 06:33:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.DeviceOwnershipRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface DeviceOwnershipRecordRepository extends JpaRepository<DeviceOwnershipRecord, Long> {
+    Optional<DeviceOwnershipRecord> findByDeviceId(String deviceId);
+}
