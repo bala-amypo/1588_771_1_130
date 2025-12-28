3a39c7682b53bd9baa468d32c62f68b7794f749b
bala-amypo <bala@amypo.in>
2025-12-28 17:47:51 +0000

    User commit

--- src/main/java/com/example/demo/util/DateUtil.java
+++ src/main/java/com/example/demo/util/DateUtil.java
@@ -1,9 +1,9 @@
-package com.example.demo.util;
+// package com.example.demo.util;
 
-import java.time.LocalDate;
+// import java.time.LocalDate;
 
-public class DateUtil {
-    public static boolean isExpired(LocalDate date) {
-        return date != null && date.isBefore(LocalDate.now());
-    }
-}
+// public class DateUtil {
+//     public static boolean isExpired(LocalDate date) {
+//         return date != null && date.isBefore(LocalDate.now());
+//     }
+// }
